e466f68c67071fe7c161f089822e1795e31b02d8
Ritik Sri <ritiksrivastav13092002@gmail.com>
2021-09-18 10:42:22 +0530

    opening ludo

--- src/app/ludo-mode-listing/ludo-mode-listing.page.ts
+++ src/app/ludo-mode-listing/ludo-mode-listing.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-ludo-mode-listing',
   templateUrl: './ludo-mode-listing.page.html',
   styleUrls: ['./ludo-mode-listing.page.scss'],
 })
-export class LudoModeListingPage implements OnInit {
+export class LudoModeListingPage implements OnInit, OnDestroy  {
 
+  public showTicTacToe = true;
+  public showContent = false;
   constructor() { }
 
+  
   ngOnInit() {
+    this.openingPage();
   }
+  openingPage(): void{
+    
+    setTimeout(()=>{
+      this.showTicTacToe = false;
+      this.showContent = true;
+      }, 5000);
+
+  }
+  ngOnDestroy(){
+    this.showTicTacToe = true;
+    this.showContent = false;
+  }
+
 
 }
