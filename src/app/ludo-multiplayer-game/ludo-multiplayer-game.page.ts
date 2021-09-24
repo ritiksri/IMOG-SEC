795d18b2f67adbfd3a2aa6810f8483ad52810722
Ritik Sri <ritiksrivastav13092002@gmail.com>
2021-09-24 19:56:23 +0530

    rs

--- src/app/ludo-multiplayer-game/ludo-multiplayer-game.page.ts
+++ src/app/ludo-multiplayer-game/ludo-multiplayer-game.page.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class LudoMultiplayerGamePage implements OnInit {
   public showLudoRandomandFrnds = false;
   public showCODE = false;
+  public showcodeedit = false;
   constructor() { }
 
   ngOnInit() {
